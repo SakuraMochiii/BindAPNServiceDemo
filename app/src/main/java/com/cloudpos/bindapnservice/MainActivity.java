835d5afba8d173867c5b4299e526de6b679bef97
yuxing.fan@wizarpos.com <yuxing.fan@wizarpos.com>
2024-05-02 18:19:05 +0800

    add call method

--- app/src/main/java/com/cloudpos/bindapnservice/MainActivity.java
+++ app/src/main/java/com/cloudpos/bindapnservice/MainActivity.java
@@ -4,21 +4,63 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.os.IBinder;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.alibaba.fastjson.JSON;
+import com.cloudpos.util.TextViewUtil;
 import com.wizarpos.wizarviewagentassistant.aidl.IAPNManagerService;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Map;
+
+public class MainActivity extends AbstractActivity implements View.OnClickListener {
+    IAPNManagerService apnManagerService ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        log_text = (TextView) this.findViewById(R.id.text_result);
+        log_text.setMovementMethod(ScrollingMovementMethod.getInstance());
+        findViewById(R.id.getSelected).setOnClickListener(this);
+        findViewById(R.id.setSelected).setOnClickListener(this);
+        findViewById(R.id.addByAllArgs).setOnClickListener(this);
+        findViewById(R.id.add).setOnClickListener(this);
+        findViewById(R.id.addByMCCAndMNC).setOnClickListener(this);
+        findViewById(R.id.clear).setOnClickListener(this);
+        findViewById(R.id.settings).setOnClickListener(this);
+        mHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == R.id.log_default) {
+                    log_text.append("\t" + msg.obj + "\n");
+                } else if (msg.what == R.id.log_success) {
+                    String str = "\t" + msg.obj + "\n";
+                    TextViewUtil.infoBlueTextView(log_text, str);
+                } else if (msg.what == R.id.log_failed) {
+                    String str = "\t" + msg.obj + "\n";
+                    TextViewUtil.infoRedTextView(log_text, str);
+                } else if (msg.what == R.id.log_clear) {
+                    log_text.setText("");
+                }
+                if(log_text.getText().length() > 256){
+                    log_text.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            log_text.scrollTo(0, log_text.getLayout().getLineTop(log_text.getLineCount()) - log_text.getHeight());
+                        }
+                    });
+                }
+            }
+        };
     }
 
     @Override
@@ -37,10 +79,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onServiceConnected(ComponentName name, IBinder service) {
                 try {
-                    IAPNManagerService apnManagerService = IAPNManagerService.Stub.asInterface(service);
+                    apnManagerService = IAPNManagerService.Stub.asInterface(service);
                     Log.d("IAPNManagerService","IAPNManagerService  bind success.");
-                    TextView view = (TextView) findViewById(R.id.text1);
-                    view.setText("IAPNManagerService  bind success.");
                 } catch (Exception e) {
                     e.printStackTrace();
                 } finally {
@@ -61,6 +101,51 @@ public class MainActivity extends AppCompatActivity {
             context.bindService(intent, connection, Context.BIND_AUTO_CREATE);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int index = v.getId();
+        String btnText = "";
+        if (v instanceof Button) {
+            Button button = (Button) v;
+            btnText = button.getText().toString();
+        }
+        try{
+            boolean flag = false;
+            writerInLog(btnText, R.id.log_default);
+            if (index == R.id.getSelected) {
+                Map map = apnManagerService.getSelected();
+                writerInSuccessLog("result is : "+ JSON.toJSONString(map));
+                return;
+            } else if (index == R.id.setSelected) {
+                flag = apnManagerService.setSelected("");
+            } else if (index == R.id.addByAllArgs) {
+                String rs = apnManagerService.addByAllArgs("","","","","","","","","","","","","","","","","","","");
+                writerInSuccessLog("result is : "+ rs);
+                return;
+            } else if (index == R.id.add) {
+                String rs = apnManagerService.add("","");
+                writerInSuccessLog("result is : "+ rs);
+                return;
+            } else if (index == R.id.addByMCCAndMNC) {
+                String rs = apnManagerService.addByMCCAndMNC("","","","");
+                writerInSuccessLog("result is : "+ rs);
+                return;
+            } else if (index == R.id.clear) {
+                flag = apnManagerService.clear();
+            } else if (index == R.id.settings) {
+                log_text.setText("");
+            }
+            if(flag){
+                writerInSuccessLog("result is true!");
+            }else if (index!=R.id.settings){
+                writerInFailedLog("result is false!");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            writerInFailedLog("test failed!");
         }
     }
 }
